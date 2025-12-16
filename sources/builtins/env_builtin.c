7a07e5320117f799010483325ac40f5e13e56489
Tim <ticharli@student.s19.be>
2025-12-16 21:54:17 +0100

    env

--- sources/builtins/env_builtin.c
+++ sources/builtins/env_builtin.c
@@ -1,26 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env_builtin.c                                      :+:      :+:    :+:   */
+/*   env_builtin.c                                      :+:      :+:    :+:   */ /*                                                                            */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcombeau <mcombeau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/17 18:55:54 by mcombeau          #+#    #+#             */
-/*   Updated: 2022/09/19 15:41:22 by mcombeau         ###   ########.fr       */
+/*   Updated: 2025/12/16 20:10:12 by ticharli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/* env_builtin:
-*	Executes the builtin env command: Prints the environment variables.
-*/
 int	env_builtin(t_data *data, char **args)
 {
 	int	i;
 
-	if (args && args[1])
-		return (errmsg_cmd("env", NULL, "too many arguments", 2));
+	if (args[1])
+	{
+		if (args[1][0] == '-')
+			return (errmsg_cmd("env", args[1], "invalid option", 125));
+		return (errmsg_cmd("env", args[1], "No such file or directory", 127));
+	}
 	i = 0;
 	if (!data->env)
 		return (EXIT_FAILURE);
